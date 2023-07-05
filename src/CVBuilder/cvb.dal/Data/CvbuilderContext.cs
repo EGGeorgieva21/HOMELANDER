1d4f61718daf4392e776105afb8c9f507f984814
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2023-07-05 11:12:30 +0300

    Scaffold database to project

---
+++ src/CVBuilder/cvb.dal/Data/CvbuilderContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using cvb.dal.Models;
+
+namespace cvb.dal.Data;
+
+public partial class CvbuilderContext : DbContext
+{
+    public CvbuilderContext()
+    {
+    }
+
+    public CvbuilderContext(DbContextOptions<CvbuilderContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cv> Cvs { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source = .\\SQLEXPRESS; initial catalog=CVBuilder; Encrypt=false; Trusted_Connection=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cv>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__CV__3214EC07FD9862B2");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Cvs).HasConstraintName("FK__CV__UserId__398D8EEE");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07CAAEB678");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ src/CVBuilder/cvb.dal/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace cvb.dal.Models;
+
+public partial class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Username { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Password { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Email { get; set; }
+
+    [InverseProperty("User")]
+    public virtual ICollection<Cv> Cvs { get; set; } = new List<Cv>();
+}

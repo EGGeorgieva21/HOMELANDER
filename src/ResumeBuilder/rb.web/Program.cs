7c785d8bcbe621e6a203838c0b734f7408dda306
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2023-07-06 20:32:04 +0300

    Resolve merge conflict

---
+++ src/ResumeBuilder/rb.web/Program.cs
@@ -0,0 +1,15 @@
+namespace rb.web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "test");
+
+            app.Run();
+        }
+    }
+}

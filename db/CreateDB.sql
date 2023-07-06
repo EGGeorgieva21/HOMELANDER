54e25ff9ba0fc2035b6b70523ed49a792a392103
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2023-07-06 12:08:42 +0300

    Scaffold database to project

--- db/CreateDB.sql
+++ db/CreateDB.sql
@@ -1,16 +1,68 @@
-CREATE DATABASE CVBuilder
+CREATE DATABASE ResumeBuilder
 GO
 
-USE CVBuilder
+USE ResumeBuilder
 
 CREATE TABLE [Users] (
 	[Id] int PRIMARY KEY IDENTITY(1,1),
-	[Username] varchar(50),
-	[Password] varchar(50),
-	[Email] varchar(50)
+	[Username] varchar(50) NOT NULL,
+	[Password] varchar(64) NOT NULL,
+	[Email] varchar(50) NOT NULL,
+	[FullName] nvarchar(50),
+	[Phone] varchar(50),
+	[Location] nvarchar(50)
+); 
+
+CREATE TABLE [Languages](
+	[Id] int PRIMARY KEY IDENTITY(1,1),
+	[Language] varchar(50) NOT NULL
 );
 
-CREATE TABLE [CV] (
+CREATE TABLE [UserLanguages](
 	[Id] int PRIMARY KEY IDENTITY(1,1),
-	[UserId] int FOREIGN KEY REFERENCES Users(Id)
+	[UserId] int FOREIGN KEY REFERENCES Users(Id) NOT NULL,
+	[LanguageId] int FOREIGN KEY REFERENCES Languages(Id) NOT NULL,
+	[AdvanceLevel] varchar(50)
+);
+
+CREATE TABLE [Skills](
+	[Id] int PRIMARY KEY IDENTITY(1,1),
+	[Skill] varchar(50)
+);
+
+CREATE TABLE [WorkExperiences](
+	[Id] int PRIMARY KEY IDENTITY(1,1),
+	[Place] nvarchar(50) NOT NULL,
+	[FromDate] date NOT NULL,
+	[ToDate] date
+);
+
+CREATE TABLE [Education](
+	[Id] int PRIMARY KEY IDENTITY(1,1),
+	[Place] nvarchar(50) NOT NULL,
+	[FromDate] date NOT NULL,
+	[ToDate] date
+);
+
+CREATE TABLE [EducationSkills](
+	[Id] int PRIMARY KEY IDENTITY(1,1),
+	[EducationId] int FOREIGN KEY REFERENCES Education(Id) NOT NULL,
+	[SkillId] int FOREIGN KEY REFERENCES Skills(Id) NOT NULL
+);
+
+CREATE TABLE [Certificates](
+	[Id] int PRIMARY KEY IDENTITY(1,1),
+	[Name] nvarchar(50),
+	[IssuedDate] date NOT NULL,
+	[ExpirationDate] date
+);
+
+CREATE TABLE [Templates](
+	[Id] int PRIMARY KEY IDENTITY(1,1)
+);
+
+CREATE TABLE [Resumes] (
+	[Id] int PRIMARY KEY IDENTITY(1,1),
+	[UserId] int FOREIGN KEY REFERENCES Users(Id) NOT NULL,
+	[TemplateId] int FOREIGN KEY REFERENCES Templates(Id) NOT NULL
 );

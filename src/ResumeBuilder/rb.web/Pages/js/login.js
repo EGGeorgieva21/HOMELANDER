fee152e31b7ac37252bc62ee8947c39d39b16b90
krisito2000 <krisi.sirakov@abv.bg>
2023-07-07 13:41:52 +0300

    register form

---
+++ src/ResumeBuilder/rb.web/Pages/js/login.js
@@ -0,0 +1,11 @@
+﻿
+function showPassword() {
+    debugger;
+    let id = document.getElementById('password');
+    if (id.type === "password") {
+        id.type = "text";
+    }
+    else {
+        id.type = "password";
+    }
+}
---
+++ src/ResumeBuilder/rb.web/Pages/js/register.js
@@ -0,0 +1,15 @@
+﻿
+
+function showPassword() {
+    debugger;
+    let id = document.getElementById('Password');
+    let bam = document.getElementById('CPassword');
+    if (id.type === "password") {
+        id.type = "text";
+        bam.type = "text";
+    } 
+    else {
+        id.type = "password";
+        bam.type = "password";    
+  }
+}

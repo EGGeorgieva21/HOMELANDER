032447e382acaafd9395ff5d142a3f10266994fd
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2023-07-13 09:51:34 +0300

    Add bearer token authorization

--- src/ResumeBuilder/rb.api/Controllers/UserController.cs
+++ src/ResumeBuilder/rb.api/Controllers/UserController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using rb.api.ViewModels;
 using rb.bll;
 using rb.dal.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace rb.api.Controllers
 {
@@ -19,16 +24,49 @@ namespace rb.api.Controllers
             userService = new UserService();
         }
 
+        [AllowAnonymous]
         [HttpPost("RegisterUser")]
-        public bool Register([FromBody] RegisterUser registerUser)
+        public ActionResult Register([FromBody] RegisterUser registerUser)
         {
-            return userService.RegisterUser(registerUser.Username, registerUser.Password, registerUser.Email);
+            User? user = userService.RegisterUser(registerUser.Username, registerUser.Password, registerUser.Email);
+
+            if (user != null)
+            {
+                var token = GenerateToken(user);
+                return Ok(token);
+            }
+            return NotFound("User not found");
         }
 
+        [AllowAnonymous]
         [HttpPost("LoginUser")]
-        public User? Login([FromBody] LoginUser loginUser)
+        public ActionResult Login([FromBody] LoginUser loginUser)
         {
-            return userService.VerifyUser(loginUser.Username, loginUser.Password);
+            User? user = userService.VerifyUser(loginUser.Username, loginUser.Password);
+
+            if(user != null)
+            {
+                var token = GenerateToken(user);
+                return Ok(token);
+            }
+            return NotFound("User not found");
+        }
+
+        private string GenerateToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("inKQxtfhWtLfkrd4hmaQeLE1TUBRgNY1"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Username)
+            };
+            var token = new JwtSecurityToken("https://localhost:7294",
+                "https://localhost:7294",
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }

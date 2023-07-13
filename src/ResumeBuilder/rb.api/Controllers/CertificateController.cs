5429eac07ce3716e341ec4db684069dbc4c8afa3
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2023-07-13 09:51:34 +0300

    Add bearer token authorization

---
+++ src/ResumeBuilder/rb.api/Controllers/CertificateController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using rb.bll;
+
+namespace rb.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CertificateController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly CertificateService certificateService;
+
+        public CertificateController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+            certificateService = new CertificateService();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Test()
+        {
+            return Ok("test");
+        }
+    }
+}
--- src/ResumeBuilder/rb.api/Program.cs
+++ src/ResumeBuilder/rb.api/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 namespace rb.api
 {
     public class Program
@@ -11,8 +17,37 @@ namespace rb.api
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ResumeBuilder", Version = "v1" });
+                c.AddSecurityDefinition("token", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    In = ParameterLocation.Header,
+                    Name = HeaderNames.Authorization,
+                    Scheme = "Bearer"
+                });
+            });
 
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true
+                };
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -23,12 +58,9 @@ namespace rb.api
             }
 
             app.UseHttpsRedirection();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }

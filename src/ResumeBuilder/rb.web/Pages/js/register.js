3a3dd59a5161ee71a2cb1aa3bb02d85e71188441
krisito2000 <krisi.sirakov@abv.bg>
2023-07-14 12:26:20 +0300

    make createCV files

--- src/ResumeBuilder/rb.web/Pages/js/register.js
+++ src/ResumeBuilder/rb.web/Pages/js/register.js
@@ -1,15 +1,10 @@
-﻿//this baby fetches data fr u
-
+﻿
+//this baby fetches data fr u
 function register() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var cPassword = document.getElementById("cPassword").value;
     var email = document.getElementById("email").value;
-
-
-    console.log(username);
-    console.log(password);
-    console.log(email);
 
     fetch('https://localhost:7294/User/RegisterUser', {
         method: 'POST',
@@ -49,26 +44,6 @@ function register() {
             console.log("Error:", error);
         });
 }
-
-
-
-
-//async function getapi(url)
-//{
-//const api_url = "https://localhost:7294/User/RegisterUser";
-//let Res = fetch(api_url);
-//    const response = await Res;
-
-
-//    var data = await response.json();
-
-//    console.log(data);
-//    if (response) {
-//        hideloader();
-//    }
-//    show(data);
-//}
-//getapi(api_url); //why call function??
 
 
 

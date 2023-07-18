2647ae79ec133c9743efd36139ddf10f50ce7b82
Georgi Zhekov <GTZhekov20@codingburgas.bg>
2023-07-18 11:35:32 +0300

    Create resume service and controller

---
+++ src/ResumeBuilder/rb.api/ViewModels/Resume/AddResume.cs
@@ -0,0 +1,11 @@
+namespace rb.api.ViewModels
+{
+    public class AddResume
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int TemplateId { get; set; }
+    }
+}
